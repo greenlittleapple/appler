5072cc99c52885c14969a829b629b350bc6278c2
greenlittleapple <main@appler.pw>
2014-09-08 19:27:23 -0700

    FIRST COMMIT!!

---
+++ modal.php
@@ -0,0 +1,25 @@
+
+<div class = "modal fade" id = "contact" role = "dialog">
+    <div class = "modal-dialog">
+        <div class = "modal-content">
+          <div class = "modal-header">
+            <h4>Contact</h4>
+          </div>
+          <div class = "modal-body">
+            <p> Add me on Skype :
+                <a href="skype:greenlittleapple890?chat"><img src="/images/skype.png" alt = "greenlittleapple890"/></a></p>
+            <p>Email me at <a href = "mailto:support@appler.pw" target="_blank">support@appler.pw</a></p>
+            <p>Add me on Steam : <a href="http://steamcommunity.com/profiles/76561198045938288/">Commander Apple</a></p>
+          </div>
+        </div>
+    </div>
+</div>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-53859915-1', 'auto');
+    ga('send', 'pageview');
+</script>
